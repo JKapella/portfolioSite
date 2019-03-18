e1ca20c9c2ce5d25ed23233b4e22321da77c9ff9
Jkapella <jvkapella@gmail.com>
2019-03-18 10:25:26 +0000

    added placeholder php for date and textarea

--- admin.php
+++ admin.php
@@ -1,6 +1,9 @@
 <?php
 
 
+$aboutMeLastUpdate = "Last updated - 09/01/1955 20:01";
+$aboutMeTextareaCopy = "I’m a Full Stack Developer-in-training with The Mayden Academy who’s enthusiastic about both front-end design and back-end systems. I’m also a Certified Scrum Master with training in Agile methodologies. I’m fascinated by games and games design, and have been jamming and creating in my spare time for several years. Away from keyboards and screens I can also be found making loud noises with electric guitars, and stroking all the good dogs.";
+
 ?>
 
 <!DOCTYPE html>
@@ -16,9 +19,11 @@
     <div class='container'>
         <h1>ADMIN <img src='img/lockicon.png' alt='an icon of a padlock'></h1>
         <section>
-            <h4>About me</h4>
-            <form action=''>
-                <textarea name='' id='' cols='30' rows='10'></textarea>
+            <h4>About me <span><?php echo $aboutMeLastUpdate; ?></span></h4>
+            <form action='' method='post'>
+                <textarea name='' id='' cols='30' rows='10'><?php echo $aboutMeTextareaCopy; ?></textarea>
+                <input type='submit'>
+                <input type='submit'>
             </form>
         </section>
     </div>
