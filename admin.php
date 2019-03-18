7f54a7ec7b884260e4ffe5ef0463538de5847ae3
Jkapella <jvkapella@gmail.com>
2019-03-18 09:11:03 +0000

    added style links to admin head, added h1

--- admin.php
+++ admin.php
@@ -6,9 +6,13 @@
 <!DOCTYPE html>
 <html lang='en'>
 <head>
-
+    <title>Admin Page</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet">
+    <link rel='stylesheet' href='normalize.css' type='text/css'>
+    <link rel='stylesheet' href='styles.css' type='text/css'>
 </head>
 <body>
-
+    <h1>ADMIN</h1>
 </body>
 </html>
