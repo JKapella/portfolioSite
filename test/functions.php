2aa226dbec2da539bf2c5a7d2c7eb4f4dab8bfcd
Jkapella <jvkapella@gmail.com>
2019-03-18 16:50:10 +0000

    added sucess test for 'editing post' function

--- test/functions.php
+++ test/functions.php
@@ -6,9 +6,12 @@ use PHPUnit\Framework\Testcase;
 
 class StackTest extends Testcase
 {
-    public function checkIfEditingPost()
+    public function testCheckIfEditingPostSuccess()
     {
-
+        $expected = false;
+        $input = [];
+        $case = checkIfEditingPost($input);
+        $this->assertEquals($expected, $case);
     }
 
 
