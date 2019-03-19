63512668c39afe3513abbb5a946a010450524567
Jkapella <jvkapella@gmail.com>
2019-03-19 15:45:39 +0000

    sorted boilerplate for login page

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+include 'functions.php';
+include 'dbConnect.php';
+
+$db = getDbConnection();
+
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>James Kapella || Web Developer</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet">
+    <link rel='stylesheet' href='normalize.css' type='text/css'>
+    <link rel='stylesheet' href='styles.css' type='text/css'>
+</head>
+<body>
+
+</body>
