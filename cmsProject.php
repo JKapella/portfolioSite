5ff5d84648afef8eb3be6c432924214d71a6ccbb
Jkapella <jvkapella@gmail.com>
2019-04-26 16:41:30 +0100

    created template for CMS project page using main index page

---
+++ cmsProject.php
@@ -0,0 +1,65 @@
+<?php
+
+include 'functions.php';
+include 'dbConnect.php';
+
+session_start();
+
+$db = getDbConnection();
+$aboutMeText = returnAboutMeTextFromDb($db);
+
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title>James Kapella || Web Developer</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400" rel="stylesheet">
+    <link rel='stylesheet' href='normalize.css' type='text/css'>
+    <link rel='stylesheet' href='styles.css' type='text/css'>
+</head>
+<body>
+    <header>
+        <div class='container'>
+            <h1>JAMES KAPELLA</h1>
+            <h3>Full stack web developer</h3>
+            <img class='scrollingFingerImage pulsingImage' src='img/scrollFingerIcon.png' alt='Scroll-down icon'>
+            <section>
+                <h4>About me</h4>
+                <p><?php echo $aboutMeText; ?></p>
+            </section>
+            <section>
+                <h4>Contact</h4>
+                <ul>
+                    <li>
+                        <a href='https://github.com/JKapella' target='_blank'>
+                            <img src='img/githubIcon.png' alt='a github icon'> Github
+                        </a>
+                    </li>
+                    <li>
+                        <a href='mailto:jvkapella@gmail.com' target='_blank'>
+                            <img src='img/emailIcon.png' alt='an email icon'> Email
+                        </a>
+                    </li>
+                    <li>
+                        <a href='http://www.linkedin.com/in/james-kapella-895001106' target='_blank'>
+                            <img src='img/linkedinIcon.png' alt='a linkedin icon'> LinkedIn
+                        </a>
+                    </li>
+                    <li>
+                        <a href='http://culturebosh.com' target='_blank'>
+                            <img src='img/gameIcon.png' alt='a games icon'> My Games
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </div>
+    </header>
+    <main>
+        <div class='container'>
+           
+        </div>
+    </main>
+</body>
+</html>
